6e83cb7e50acaa9d3873a6ccf8ae2b9c6098b8bf
Boersma, Dustin <dustin.boersma@gmail.com>
2017-10-04 17:16:04 -0700

    syntax fix

--- models/Snapshot.js
+++ models/Snapshot.js
@@ -1,12 +1,12 @@
 var client = require('./client');
 
-var GameSnapshot = function(_time, options) {
+var Snapshot = function(_time, options) {
     this._time = _time;
     this.channels = options.channels;
     this.viewers = options.viewers;
 };
 
-GameSnapshot.prototype._insert = function() {
+Snapshot.prototype._insert = function() {
     return new Promise((resolve, reject) => {
         var queryText = 'INSERT INTO snapshots(_time, channels, viewers) VALUES($1, $2, $3)';
         client.query(queryText, [this._time, this.channels, this.viewers], function(err, res) {
@@ -16,11 +16,11 @@ GameSnapshot.prototype._insert = function() {
     });
 };
 
-GameSnapshot.prototype.save = function() {
+Snapshot.prototype.save = function() {
     return new Promise((resolve, reject) => {
         this._insert()
             .then(resolve, reject);
     });
 };
 
-module.exports = GameSnapshot;
+module.exports = Snapshot;
