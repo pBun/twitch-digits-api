bed147bbe44e5390947e9679af7acb27b2d7f8e1
Boersma, Dustin <dustin.boersma@gmail.com>
2017-10-05 14:09:54 -0700

    fixed relative path issue

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './public_src/index.jsx',
     output: {
         path: path.resolve(__dirname, './public'),
-        publicPath: '/public/',
+        publicPath: '/',
         filename: 'app.js'
     },
     module: {
