5135767e0715ad84624dda50d397482371175fa0
Boersma, Dustin <dustin.boersma@gmail.com>
2017-10-08 01:19:48 -0700

    enabled cors for digits2.twitch and localhost

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,20 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
+
+
+var whitelist = ['digits2.twitch.pub']
+var corsOptions = {
+    origin: function(origin, callback) {
+        if (origin.indexOf('localhost') !== -1 || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+router.use(cors(corsOptions));
+
 
 router.get('/test', function(req, res) {
     res.json({ importantMessage: 'what up fam?' });
